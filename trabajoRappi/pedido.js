8086f66aee08f980130ade2f165c66a83a1292f3
Bryan Camilo Diaz Sanchez <brayancamilodiaz3@gmail.com>
2026-06-26 12:59:15 -0500

    subiendo archivos por primera vez

---
+++ trabajoRappi/pedido.js
@@ -0,0 +1,10 @@
+let NombreCliente = "Carlos Perez"
+const CiudadCliente = "Bogota"
+let ClienteEsPrimun = 
+
+
+if (NombreCliente === "Carlos Perez") { 
+    
+}
+
+console.log ("Hola", NombreCliente , " Tu pedido a ", CiudadCliente , ClienteEsPrimun )
